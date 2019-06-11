f18fb1a2feb6c02a7367c2d6aca37b0d6f28a521
Mathias Payan <mathias.payan@meritis.fr>
2019-06-11 23:26:06 +0200

    Add Zobrist hash

--- game_theory/src/main/scala/checkers/Board.scala
+++ game_theory/src/main/scala/checkers/Board.scala
@@ -3,25 +3,49 @@ package checkers
 import Pos._
 
 
-case class Board(pieces: Map[Pos, Piece]) {
+case class Board(pieces: Map[Pos, Piece], hash: Long) {
 
 	def apply(at: Pos): Option[Piece] = pieces get at
 
 	def place(piece: Piece, at: Pos): Option[Board] = {
 		if (pieces contains at) None
-		else Some(copy(pieces = pieces + ((at, piece))))
+		else Some(copy(pieces = pieces + ((at, piece)),
+						hash = updateHash(hash, (at, Some(piece)))))
 	}
 
-	def remove(at: Pos): Option[Board] = pieces get at map { piece => copy(pieces = pieces - at) }
+	def replace(piece: Piece, at: Pos): Option[Board] = {
+		if (!(pieces contains at)) None
+		else Some(copy(pieces = pieces - at + ((at, piece)),
+						hash = updateHash(hash, (at, Some(piece)))))
+	}
+
+	def remove(at: Pos): Option[Board] = {
+		pieces get at map { piece =>
+			copy(pieces = pieces - at,
+					hash = updateHash(hash, (at, None)))
+		}
+	}
 
 	def move(orig: Pos, dest: Pos): Option[Board] = {
 		if (pieces contains dest) None
-		else pieces get orig map { piece => copy(pieces = pieces - orig + ((dest, piece))) }
+		else pieces get orig map { piece =>
+			copy(pieces = pieces - orig + ((dest, piece)),
+					hash = List((orig, None), (dest, Some(piece))).foldLeft(hash)(updateHash))
+		}
 	}
 
 	def take(orig: Pos, dest: Pos, taken: Pos): Option[Board] = {
-		if ((pieces contains taken) && !(pieces contains dest)) pieces get orig map { piece => copy(pieces = pieces - orig - taken + ((dest, piece))) }
-		else None
+		if ((pieces contains taken) && !(pieces contains dest) && (pieces contains orig)) {
+			pieces get orig map { piece =>
+				copy(pieces = pieces - orig - taken + ((dest, piece)),
+						hash = List((orig, None), (taken, None), (dest, Some(piece))).foldLeft(hash)(updateHash))
+			}
+
+		} else None
+	}
+
+	def updateHash(acc: Long, elt: (Pos, Option[Piece])): Long = {
+		ZobristHash.replace(acc, elt._1, pieces.get(elt._1), elt._2)
 	}
 
 }
@@ -33,9 +57,13 @@ object Board {
 		val blackPart = for (m <- 1 to 20) yield (posAt(m).get, Piece(Black, Pawn))
 		val whitePart = for (m <- 31 to 50) yield (posAt(m).get, Piece(White, Pawn))
 
-		Board((blackPart ++ whitePart).toMap)
+		val map = (blackPart ++ whitePart).toMap
+		Board(map, ZobristHash.computeHash(map))
 	}
 
-	def empty: Board = Board(Map.empty[Pos, Piece])
+	def empty: Board = {
+		val map = Map.empty[Pos, Piece]
+		Board(map, ZobristHash.computeHash(map))
+	}
 
 }
